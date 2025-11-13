b155e587ef9ce3b163dc39aa5dbb5011d95b1fd0
rebecca bossé <rebeccabosse@r.local>
2025-11-13 17:09:33 +0100

    remove npm pokemon

--- app/page.js
+++ app/page.js
@@ -1,12 +1,13 @@
 import MainHeader from "@/components/main-header/main-header";
 import "../styles/globals.css";
+import PokemonList from "./pokemons/pokemonList";
 export default function Home() {
   return (
     <>
       <MainHeader />
       <h1 className="title">Welcome to the Pokedex</h1>
       <div className="content">
-        <p>This is the home page of the Pokedex application.</p>
+        <PokemonList />
       </div>
     </>
   );
---
+++ app/pokemons/pokemonList.js
@@ -0,0 +1,10 @@
+function PokemonList() {
+  return (
+    <div>
+      <h1>Pokemon List</h1>
+      {/* Add your Pokemon list rendering logic here */}
+    </div>
+  );
+}
+
+export default PokemonList;
